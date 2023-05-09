67996410b53e6da137e7ffef64e09b46bb194ee0
Skyenet <skye@skyenet.online>
2023-05-08 23:54:44 -0400

    Dotfile updates

--- init.el
+++ init.el
@@ -34,6 +34,20 @@
 ;; workaround bug in Emacs 26.2
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 
+;; Setup Straight package manager
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 ;; Setting up the package manager. Install if missing.
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -55,3 +69,4 @@
 
 (provide 'init)
 ;;; init.el ends here
+(put 'upcase-region 'disabled nil)
