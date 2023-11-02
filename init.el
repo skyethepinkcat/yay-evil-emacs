a48b005349f661da5354f9b45333b09e9b42e1ca
Skye J <skye@skyenet.online>
2023-11-02 17:23:33 -0400

    updates

--- init.el
+++ init.el
@@ -70,17 +70,6 @@
 (provide 'init)
 ;;; init.el ends here
 (put 'upcase-region 'disabled nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("993aac313027a1d6e70d45b98e121492c1b00a0daa5a8629788ed7d523fe62c1" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "570263442ce6735821600ec74a9b032bc5512ed4539faf61168f2fdf747e0668" "e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" default))
- '(org-bullets-bullet-list '("✿" "✸" "◉" "○")))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
