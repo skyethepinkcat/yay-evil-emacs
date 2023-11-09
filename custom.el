ee340fb48787a91cfe2fd754c6e6111be4dc3ba1
Skye J <skye@skyenet.online>
2023-11-09 11:42:10 -0500

    Add custom.el and pseudocode.el

---
+++ custom.el
@@ -0,0 +1,22 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("993aac313027a1d6e70d45b98e121492c1b00a0daa5a8629788ed7d523fe62c1" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "570263442ce6735821600ec74a9b032bc5512ed4539faf61168f2fdf747e0668" "e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" default))
+ '(lsp-keymap-prefix "C-c l")
+ '(org-bullets-bullet-list '("✿" "✸" "◉" "○"))
+ '(tab-bar-format
+   '(tab-bar-format-tabs tab-bar-separator tab-bar-format-add-tab tab-bar-format-align-right tab-bar-format-global))
+ '(tab-bar-history-mode nil)
+ '(tab-bar-mode t)
+ '(tab-bar-new-tab-choice "*dashboard*")
+ '(tab-bar-show t)
+ '(tab-bar-tab-name-function 'tab-bar-tab-name-truncated))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
