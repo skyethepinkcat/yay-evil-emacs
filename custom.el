a6b168ba1f56f8bd4870325fe069941e72925e3b
Skye J <skye@skyenet.online>
2024-01-22 09:25:27 -0500

    Updates

--- custom.el
+++ custom.el
@@ -7,6 +7,9 @@
    '("993aac313027a1d6e70d45b98e121492c1b00a0daa5a8629788ed7d523fe62c1" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "570263442ce6735821600ec74a9b032bc5512ed4539faf61168f2fdf747e0668" "e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" default))
  '(lsp-keymap-prefix "C-c l")
  '(org-bullets-bullet-list '("✿" "✸" "◉" "○"))
+ '(org-export-backends '(ascii html icalendar latex md odt))
+ '(org-format-latex-header
+   "\\documentclass{article}\12\\usepackage[usenames]{color}\12[DEFAULT-PACKAGES]\12[PACKAGES]\12\\pagestyle{empty}             % do not remove\12% The settings below are copied from fullpage.sty\12\\setlength{\\textwidth}{\\paperwidth}\12\\addtolength{\\textwidth}{-3cm}\12\\setlength{\\oddsidemargin}{1.5cm}\12\\addtolength{\\oddsidemargin}{-2.54cm}\12\\setlength{\\evensidemargin}{\\oddsidemargin}\12\\setlength{\\textheight}{\\paperheight}\12\\addtolength{\\textheight}{-\\headheight}\12\\addtolength{\\textheight}{-\\headsep}\12\\addtolength{\\textheight}{-\\footskip}\12\\addtolength{\\textheight}{-3cm}\12\\setlength{\\topmargin}{1.5cm}\12\\addtolength{\\topmargin}{-2.54cm}\12\\usepackage{nicefrac}\12\\newcommand{\\floor}[1]{\\lfloor #1 \\rfloor}")
  '(tab-bar-format
    '(tab-bar-format-tabs tab-bar-separator tab-bar-format-add-tab tab-bar-format-align-right tab-bar-format-global))
  '(tab-bar-history-mode nil)
